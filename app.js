5ed161fe8ab058b4ded32c700c7d08a36780e6f2
Marin, Adrian <adrian.marin@sogeti.com>
2019-09-18 15:11:01 +0200

    Removed comments

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-// NodeJS modules for Express app - installed with NPM
+
 var express = require('express');
 var http = require('http');
 
-// Create the Express app 
 var app = express();
-
-// google-cloud/datastore
 const model = require('./database/cloud-datastore');
 
-/** Test Endpoint */
-// https://8080-dot-8699478-dot-devshell.appspot.com/getcustomers
-app.get('/test', async(req, res) => {
-    res.send('Greetings from the Test controller!');
-});
-
-// https://8080-dot-8699478-dot-devshell.appspot.com/getcustomer/?id=5629499534213120
 app.get('/getcustomers', async (req, res, next) => {
     model.list(10, (err, entities) => {
         if (err) {
@@ -27,7 +17,6 @@ app.get('/getcustomers', async (req, res, next) => {
     });
 });
 
-https://8080-dot-8699478-dot-devshell.appspot.com/getcustomer/?id=5629499534213120
 app.get('/getcustomer', async (req, res, next) => {
     model.read(req.query.id, (err, entity) => {
         if (err) {
@@ -39,8 +28,6 @@ app.get('/getcustomer', async (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    // Format error and forward to generic error handler for logging and
-    // responding to the request
     err.response = {
       message: err.message,
       internalCode: err.code,
@@ -51,16 +38,10 @@ app.use((err, req, res, next) => {
 var port = (process.env.PORT || '8080');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 var server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen( port, function(){
-    console.log("Express Server Runing on port"+ app.get('port'));
+    console.log("Express Server Runing on port "+ app.get('port'));
 });
+
 module.exports = app;
